70e0a04c97e069ad5be00117dec8f34ffae908bf
Dhalintin <99981753+Dhalintin@users.noreply.github.com>
2022-07-25 11:57:25 +0300

    Creating a link list

--- linkedlist.php
+++ linkedlist.php
@@ -3,35 +3,60 @@
 class Node
 {
     public $value;
-    
-    public function __construct($value,  public ?Node $next = null) { }
-} 
+    public $next;
 
+    public function __construct($value, $next = null)
+    {
+        $this->value = $value;
+        $this->next = $next;
+    }
+}
 
-class LinkedList
-{
+class LinkedList{
+    //Initilalizing the  Node value to a nullable node type
     public ?Node $head = null;
 
-    public function __construct(?Node $head = null) { }
- 
-    public function print()
+    //Function to add value to the linkedlist
+    public function addValue($value): Node
     {
-        $node = $this->head;
- 
-        while ($node) {
-            echo $node->value;
-        }
-        echo "\n";
-    }
-
-    public function addValue($value): Node {
         $this->head = $node = new Node($value, $this->head);
      
         return $node;
     }
+
+    //Function to Print out value 
+    public function print()
+    {
+        $node = $this->head;
+
+        while($node){
+            echo $node->value;
+            if($node = $node->next){
+                echo "->";
+            }
+        }
+    }
 }
 
-$a = new LinkedList();
 
-$a->addValue('A');
-$a->print(); // A
+//Test Case
+$list = new LinkedList();
+ 
+$a = $list->addValue('A');
+$b = $list->addValue('B');
+$c = $list->addValue('C');
+$d = $list->addValue('D');
+$e = $list->addValue('E');
+$f = $list->addValue('F');
+$g = $list->addValue('G');
+$h = $list->addValue('H');
+
+
+$list->print(); 
+
+/*Expected Output
+
+    H->G->F->E->D->C->B->A
+
+*/
+
